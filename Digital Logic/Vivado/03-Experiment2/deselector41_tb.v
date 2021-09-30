c2e224b3dc4f1b645c1701ee59384d561c20155a
lphlch <lvpinhao@sohu.com>
2021-09-30 21:24:19 +0800

    bugfix

--- Digital Logic/Vivado/03-Experiment2/deselector41_tb.v
+++ Digital Logic/Vivado/03-Experiment2/deselector41_tb.v
@@ -1,24 +1,20 @@
 `timescale 1ns/1ns
 module deselector41_tb;
     reg iS1,iS0,iC;
-    reg oZ0,oZ1,oZ2,oZ3;
-    deselector41 uut(iC,iS1,iS0,oZ0,oZ1,oZ2,oZ3);
+    wire oZ0,oZ1,oZ2,oZ3;
+    de_selector41 uut(iC,iS1,iS0,oZ0,oZ1,oZ2,oZ3);
     initial
     begin
         iS1=0;
-        iC=0;
-        #40 iS1=0;
-        #40 iS1=0;
-        #40 iS1=1;
-        #40 iS1=1;
-    end
-    initial
-    begin
         iS0=0;
         iC=0;
-        #40 iS0=0;
-        #40 iS0=1;
-        #40 iS0=0;
-        #40 iS0=1;
+        #40 iS1=0;
+        iS0=0;
+        #40 iS1=0;
+        iS0=1;
+        #40 iS1=1;
+        iS0=0;
+        #40 iS1=1;
+        iS0=1;
     end
 endmodule
