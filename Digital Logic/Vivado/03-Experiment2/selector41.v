11d3704d58c75cf8b9eed2af3939030084c01f14
lphlch <lvpinhao@sohu.com>
2021-09-30 20:38:32 +0800

    Exp2-1 选择器位数扩展

--- Digital Logic/Vivado/03-Experiment2/selector41.v
+++ Digital Logic/Vivado/03-Experiment2/selector41.v
@@ -7,5 +7,9 @@ module selector41(
     input iS0,
     output [3:0] oZ
 );
-    assign oZ=iC0 & ~iS0 & ~iS1 | iC1 & ~iS1 & iS0 | iC2 & iS1 & ~iS0 | iC3 & iS1 &iS0;
+wire [3:0]iS14,iS04;
+assign iS14={{3{iS1}},iS1};
+assign iS04={{3{iS0}},iS0};
+
+    assign oZ=(iC0 & ~iS04 & ~iS14) |( iC1 & ~iS14 & iS04) |( iC2 & iS14 & ~iS04) | (iC3 & iS14 &iS04);
 endmodule
--- Digital Logic/Vivado/03-Experiment2/selector41_tb.v
+++ Digital Logic/Vivado/03-Experiment2/selector41_tb.v
@@ -3,7 +3,8 @@ module selector41_tb;
     reg iS1;
     reg iS0;
     reg [3:0]iC0,iC1,iC2,iC3;
-    wire oZ;
+    wire [3:0]oZ;
+    selector41 uut(iC0,iC1,iC2,iC3,iS1,iS0,oZ);
     initial
     begin
         iS1=0;
@@ -15,58 +16,14 @@ module selector41_tb;
     initial
     begin
         iS0=0;
+        iC0=4'b0001;
+        iC1=4'b0011;
+        iC2=4'b0111;
+        iC3=4'b1111;
         #40 iS0=0;
         #40 iS0=1;
         #40 iS0=0;
         #40 iS0=1;
     end
-    initial
-    begin
-        iC0=0;
-        #20 iC0=0;
-        #20 iC0=1;
-        #20 iC0=0;
-        #20 iC0=1;
-        #20 iC0=0;
-        #20 iC0=1;
-        #20 iC0=0;
-        #20 iC0=1;
-    end
-        initial
-    begin
-        iC1=0;
-        #20 iC1=0;
-        #20 iC1=1;
-        #20 iC1=0;
-        #20 iC1=1;
-        #20 iC1=0;
-        #20 iC1=1;
-        #20 iC1=0;
-        #20 iC1=1;
-    end
-        initial
-    begin
-        iC2=0;
-        #20 iC2=0;
-        #20 iC2=1;
-        #20 iC2=0;
-        #20 iC2=1;
-        #20 iC2=0;
-        #20 iC2=1;
-        #20 iC2=0;
-        #20 iC2=1;
-    end
-            initial
-    begin
-        iC3=0;
-        #20 iC3=0;
-        #20 iC3=1;
-        #20 iC3=0;
-        #20 iC3=1;
-        #20 iC3=0;
-        #20 iC3=1;
-        #20 iC3=0;
-        #20 iC3=1;
-    end
-    selector41 uut(iC0,iC1,iC2,iC3,iS1,iS0,oZ);
+
 endmodule
