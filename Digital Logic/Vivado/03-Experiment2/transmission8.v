8a83dab77ae4ecc5856e968a8e7dec7354b2baa6
lphlch <lvpinhao@sohu.com>
2021-10-06 14:12:51 +0800

    8bits to 1bit

--- Digital Logic/Vivado/03-Experiment2/transmission8.v
+++ Digital Logic/Vivado/03-Experiment2/transmission8.v
@@ -3,27 +3,22 @@ module transmission8(
     input A,B,C,
     output [7:0] oData
 );
-    wire [7:0]iS2,iS1,iS0;
-    assign iS2={{7{A}},A};
-    assign iS1={{7{B}},B};
-    assign iS0={{7{C}},C};
 
-    wire [7:0]Y;
-    assign Y=   (iData[0] & ~iS0 & ~iS1 & ~iS2) | 
-                (iData[1] & iS0 & ~iS1 & ~iS2) |
-                (iData[2] & ~iS0 & iS1 & ~iS2) | 
-                (iData[3] & iS0 & iS1 & ~iS2) | 
-                (iData[4] & ~iS0 & ~iS1 & iS2) | 
-                (iData[5] & iS0 & ~iS1 & iS2) | 
-                (iData[6] & ~iS0 & iS1 & iS2) | 
-                (iData[7] & iS0 & iS1 & iS2)
+    assign Y=  ( (iData[0] & ~C & ~B & ~A) | 
+                (iData[1] & C & ~B & ~A) |
+                (iData[2] & ~C & B & ~A) | 
+                (iData[3] & C & B & ~A) | 
+                (iData[4] & ~C & ~B & A) | 
+                (iData[5] & C & ~B & A) | 
+                (iData[6] & ~C & B & A) | 
+                (iData[7] & C & B & A));
 
-    assign oData[0]= Y | iS2 | iS1 | iS0;
-    assign oData[1]= Y | iS2 | iS1 | ~iS0;
-    assign oData[2]= Y | iS2 | ~iS1 | iS0;
-    assign oData[3]= Y | iS2 | ~iS1 | ~iS0;
-    assign oData[4]= Y | ~iS2 | iS1 | iS0;
-    assign oData[5]= Y | ~iS2 | iS1 | ~iS0;
-    assign oData[6]= Y | ~iS2 | ~iS1 | iS0;
-    assign oData[7]= Y | ~iS2 | ~iS1 | ~iS0;
+    assign oData[0]= Y | A | B | C;
+    assign oData[1]= Y | A | B | ~C;
+    assign oData[2]= Y | A | ~B | C;
+    assign oData[3]= Y | A | ~B | ~C;
+    assign oData[4]= Y | ~A | B | C;
+    assign oData[5]= Y | ~A | B | ~C;
+    assign oData[6]= Y | ~A | ~B | C;
+    assign oData[7]= Y | ~A | ~B | ~C;
 endmodule
