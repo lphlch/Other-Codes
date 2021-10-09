078ee564efa34af74e0ffd46f0219bdcaea928dd
lphlch <lvpinhao@sohu.com>
2021-10-09 20:45:00 +0800

    Exp3-4
    test bugfix

--- Digital Logic/Vivado/04-Experiment3/encoder83_Pri_tb.v
+++ Digital Logic/Vivado/04-Experiment3/encoder83_Pri_tb.v
@@ -8,25 +8,25 @@ module encoder83_Pri_tb ();
     initial
     begin
         #50
-        iData=8'b00000000;
-        #50 iData=8'b00000001;
-        #50 iData=8'b00000010;
-        #50 iData=8'b00000100;
-        #50 iData=8'b00001000;
-        #50 iData=8'b00010000;
-        #50 iData=8'b00100000;
-        #50 iData=8'b01000000;
-        #50 iData=8'b10000000;
+        iData=8'b11111111;
+        #50 iData=8'b11111110;
+        #50 iData=8'b11111101;
+        #50 iData=8'b11111011;
+        #50 iData=8'b11110111;
+        #50 iData=8'b11101111;
+        #50 iData=8'b11011111;
+        #50 iData=8'b10111111;
+        #50 iData=8'b01111111;
 
-        #50 iData=8'b00000000;
-        #50 iData=8'b00000001;
-        #50 iData=8'b00000010;
-        #50 iData=8'b00000100;
-        #50 iData=8'b00001000;
-        #50 iData=8'b00010000;
-        #50 iData=8'b00100000;
-        #50 iData=8'b01000000;
-        #50 iData=8'b10000000;
+        #50 iData=8'b11111111;
+        #50 iData=8'b11111110;
+        #50 iData=8'b11111101;
+        #50 iData=8'b11111011;
+        #50 iData=8'b11110111;
+        #50 iData=8'b11101111;
+        #50 iData=8'b11011111;
+        #50 iData=8'b10111111;
+        #50 iData=8'b01111111;
     end
 
     initial
