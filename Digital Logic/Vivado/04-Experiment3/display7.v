1e0df18bd4740936b2d25b6650b906dc478c4636
lphlch <lvpinhao@sohu.com>
2021-10-09 19:47:27 +0800

    Exp3-2
    e显示错误
    v1.1

--- Digital Logic/Vivado/04-Experiment3/display7.v
+++ Digital Logic/Vivado/04-Experiment3/display7.v
@@ -6,7 +6,7 @@ module display7 (
     assign oData[1]=(~iData[3] & iData[2] & ~iData[1] & iData[0]) | (~iData[3] & iData[2] & iData[1] & ~iData[0]);
     assign oData[2]=(~iData[3] & ~iData[2] & iData[1] & ~iData[0]);
     assign oData[3]=(~iData[3] & ~iData[2] & ~iData[1] & iData[0]) | (~iData[3] & iData[2] & ~iData[1] & ~iData[0]) | (~iData[3] & iData[2] & iData[1] & iData[0]);
-    assign oData[4]=(~iData[3] & ~iData[2] & iData[0]) | (~iData[3] & iData[2] & iData[1]) | (~iData[3] & iData[2] & iData[1] & iData[0]) | (iData[3] & ~iData[2] & ~iData[1] & iData[0]);
+    assign oData[4]=(~iData[3] & ~iData[2] & ~iData[1]) | (~iData[3] & iData[2] & iData[1]) | (~iData[3] & iData[2] & iData[1] & iData[0]) | (iData[3] & ~iData[2] & ~iData[1] & iData[0]);
     assign oData[5]=(~iData[3] & ~iData[2] & ~iData[1] & iData[0]) | (~iData[3] & ~iData[2] & iData[1]) | (~iData[3] & iData[2] & iData[1] & iData[0]);
     assign oData[6]=(~iData[3] & ~iData[2] & ~iData[1]) | (~iData[3] & iData[2] & iData[1] & iData[0]);
 endmodule
