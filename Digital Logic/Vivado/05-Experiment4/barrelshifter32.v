36843542a412aac61ec67885fc6384c4edb05408
lphlch <lvpinhao@sohu.com>
2021-10-19 17:04:12 +0800

    Update barrelshifter32.v
    bugfix

--- Digital Logic/Vivado/05-Experiment4/barrelshifter32.v
+++ Digital Logic/Vivado/05-Experiment4/barrelshifter32.v
@@ -7,25 +7,26 @@ always @(*) begin
     case (aluc)
         2'b00: begin
             temp = b[0]?{a[31],a[31:1]}:a;
-            temp = b[1]?{{2{temp[31]}},temp[31:2]}:temp;
-            temp = b[2]?{{4{temp[31]}},temp[31:4]}:temp;
-            temp = b[3]?{{8{temp[31]}},temp[31:8]}:temp;
-            temp = b[4]?{{16{temp[31]}},temp[31:16]}:temp;
+            temp = b[1]?{{2{a[31]}},temp[31:2]}:temp;
+            temp = b[2]?{{4{a[31]}},temp[31:4]}:temp;
+            temp = b[3]?{{8{a[31]}},temp[31:8]}:temp;
+            temp = b[4]?{{16{a[31]}},temp[31:16]}:temp;
+            
         end
         2'b10:begin
-            temp = b[0]?{1'b0,temp[31:1]}:a;
-            temp = b[1]?{{2{1'b0}},a[31:2]}:temp;
-            temp = b[2]?{{4{1'b0}},a[31:4]}:temp;
-            temp = b[3]?{{8{1'b0}},a[31:8]}:temp;
-            temp = b[4]?{{16{1'b0}},a[31:16]}:temp;
+            temp = b[0]?{1'b0,a[31:1]}:a;
+            temp = b[1]?{{2{1'b0}},temp[31:2]}:temp;
+            temp = b[2]?{{4{1'b0}},temp[31:4]}:temp;
+            temp = b[3]?{{8{1'b0}},temp[31:8]}:temp;
+            temp = b[4]?{{16{1'b0}},temp[31:16]}:temp;
         end
         //11 or 01
         default: begin
             temp = b[0]?{a[30:0],1'b0}:a;
-            temp = b[1]?{a[29:0],{2{1'b0}}}:temp;
-            temp = b[2]?{a[27:0],{4{1'b0}}}:temp;
-            temp = b[3]?{a[23:0],{8{1'b0}}}:temp;
-            temp = b[4]?{a[15:0],{16{1'b0}}}:temp;
+            temp = b[1]?{temp[29:0],{2{1'b0}}}:temp;
+            temp = b[2]?{temp[27:0],{4{1'b0}}}:temp;
+            temp = b[3]?{temp[23:0],{8{1'b0}}}:temp;
+            temp = b[4]?{temp[15:0],{16{1'b0}}}:temp;
         end
     endcase
     c = temp;
