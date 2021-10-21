dffefa1ba45b52aeed07815f411c951932bbaaf4
lphlch <lvpinhao@sohu.com>
2021-10-21 19:56:14 +0800

    Exp5-3
    v1.1
    Tested

--- Digital Logic/Vivado/06-Experiment5/FA.v
+++ Digital Logic/Vivado/06-Experiment5/FA.v
@@ -5,7 +5,7 @@ module FA (
     output oS,
     output oC
 );
-    reg aAndb,aXorb,cAndxor
+    wire aAndb,aXorb,cAndxor;
     and and1(aAndb,iA,iB);
     xor xor1(aXorb,iA,iB);
     and and2(cAndxor,iC,aXorb);
