a6ad9072de73d3d0695f5edd650a05563b34fbe2
lphlch <lvpinhao@sohu.com>
2021-11-04 18:47:42 +0800

    Exp7-2

--- Digital Logic/Vivado/08-Experiment7/Divider.v
+++ Digital Logic/Vivado/08-Experiment7/Divider.v
@@ -1,7 +1,24 @@
-module Divider (
-    input I_CLK,
-    input rst,
-    output O_CLK
-);
+module Divider (input I_CLK,
+                input rst,
+                output O_CLK);
     
+    parameter i = 20;
+    reg[31:0] counter;
+    reg outputT;
+    always @(posedge I_CLK or posedge rst) begin
+        if (rst) begin
+            counter <= 0;
+            outputT <= 0;
+        end
+        else if (counter == i) begin
+            counter <= 0;
+            outputT   <= 1;
+        end
+        else begin
+            counter <= counter + 1;
+            outputT   <= 0;
+        end
+    end
+    
+    assign O_CLK = outputT;
 endmodule
---
+++ Digital Logic/Vivado/08-Experiment7/Divider_tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ns
+module Divider_tb ();
+    reg clk,reset;
+    wire oclk;
+    
+    Divider uut (clk,reset,oclk);
+    
+    initial begin
+        clk = 0;
+        repeat(300) begin
+            #3 clk = ~clk;
+        end
+    end
+    
+    initial begin
+        reset = 1;
+        #20
+        reset = 0;
+        #500
+        reset = 1;
+    end
+endmodule
