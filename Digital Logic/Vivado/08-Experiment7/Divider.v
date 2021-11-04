76e6d56c184eedc76dc2035df1e34950bb859b24
lphlch <lvpinhao@sohu.com>
2021-11-04 20:48:33 +0800

    Update Divider.v

--- Digital Logic/Vivado/08-Experiment7/Divider.v
+++ Digital Logic/Vivado/08-Experiment7/Divider.v
@@ -2,28 +2,23 @@ module Divider (input I_CLK,
                 input rst,
                 output O_CLK);
     
-    parameter i = 20;
-    reg[31:0] counter=0;
-    reg outputT=0;
-    always @(posedge I_CLK or posedge rst) begin
+    parameter i       = 20;
+    reg[31:0] counter = 0;
+    reg outputT       = 0;
+    always @(posedge I_CLK or posedge rst)
         if (rst) begin
-            counter <= 0;
-            outputT <= 0;
+            counter = 0;
+            outputT   = 1;
         end
-        else if (counter < i/2) begin
-            counter <= counter + 1;
-            outputT <= 0;
-        end
-        else if (counter ==i) begin
-            counter <= 1;
-            outputT <= 0;
+        else if (counter < i / 2 ) begin
+            counter = counter + 1;
+            outputT   = outputT;
         end
         else begin
-            counter <= counter + 1;
-            outputT <= 1;
+            counter = 1;
+            outputT   = ~outputT;
         end
-    end
     
     assign O_CLK = outputT;
 endmodule
- 
+    
