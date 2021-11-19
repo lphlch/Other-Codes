a552c87a83aaab92a8887d74098c0e57794bb766
lphlch <lvpinhao@sohu.com>
2021-11-20 00:31:50 +0800

    Update HW4-4.cpp
    邻接表版

--- Data Structure/HW4-4.cpp
+++ Data Structure/HW4-4.cpp
@@ -5,47 +5,65 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <time.h>
 const int INTMAX = 2147483647;
 using namespace std;
-vector<int> path;	//save the previous vertex of the way that to the start vertex
-vector<int> shortestPath;	//save the shortest path from the start vertex to the other vertex
-#pragma region DIRECTED_GRAPH
-class DirectedGraph
-{
-protected:
-	vector< vector< int > > matrix; //save the graph matrix
-	int vertexNum;
-	int edgeNum;
 
-public:
-	void create();
+struct Edge
+{
+	int vertexId;
+	int weight;
 };
 
-#pragma endregion
+int findEdge(vector<vector<Edge>>& table, int start, int end)
+{
+	for (int i = 0; i < table[start].size(); i++)
+	{
+		if (table[start][i].vertexId == end)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
 
-#pragma region UNDIRECTED_NETWORK
-//inherit from DirectedGraph
-class UndirectedNetwork : public DirectedGraph
+time_t searchMinTime = 0;
+time_t updateTime = 0;
+time_t createTime = 0;
+time_t tmpTime;
+
+class UndirectedNetwork
 {
 public:
+	vector<vector<Edge>> table;
+	int vertexNum;
+	int edgeNum;
+	int startVertex;
 	void create();
 	void allShortestPath(vector<int>& path, vector<int>& shortestPath);
-	int startVertex;
 };
 
 void UndirectedNetwork::create()
 {
 	cin >> vertexNum >> edgeNum >> startVertex;
-
+	table.resize(vertexNum);
 	int a, b;
 	int weight;
+	int tmpEdgeIndex;
 	for (int i = 0; i < edgeNum; i++)
 	{
 		cin >> a >> b >> weight;
-		if (matrix[a - 1][b - 1] > weight)
+		int indexA = a - 1;
+		int indexB = b - 1;
+		tmpEdgeIndex = findEdge(table, indexA, indexB);
+		if (tmpEdgeIndex == -1)
 		{
-			matrix[a - 1][b - 1] = weight; //set the matrix
-			matrix[b - 1][a - 1] = weight; //symmetric matrix
+			table[indexA].push_back({ indexB, weight });
+			//table[indexB].push_back({ indexA, weight });
+		}
+		else if (table[indexA][tmpEdgeIndex].weight > weight)
+		{
+			table[indexA][tmpEdgeIndex].weight = weight;
 		}
 	}
 }
@@ -62,11 +80,20 @@ void UndirectedNetwork::allShortestPath(vector<int>& path, vector<int>& shortest
 	shortestPath.resize(vertexNum, INTMAX);
 	path.clear();
 	path.resize(vertexNum);
-
+	int tmpEdgeIndex = -1;
 	for (int i = 0; i < vertexNum; i++)
 	{
-		shortestPath[i] = matrix[start][i]; //initialize the shortestPath of start
-		if (shortestPath[i] != INTMAX)			//if the start vertex is connected to the i vertex
+		tmpEdgeIndex = findEdge(table, start, i);
+		if (tmpEdgeIndex == -1) //initialize the shortestPath of start
+		{
+			shortestPath[i] = INTMAX;
+		}
+		else
+		{
+			shortestPath[i] = table[start][tmpEdgeIndex].weight;
+		}
+
+		if (shortestPath[i] != INTMAX) //if the start vertex is connected to the i vertex
 		{
 			path[i] = start; //set the path of start vertex to i vertex
 		}
@@ -80,6 +107,7 @@ void UndirectedNetwork::allShortestPath(vector<int>& path, vector<int>& shortest
 
 	for (int i = 1; i < vertexNum; i++)
 	{
+		tmpTime = clock();
 		int minPath = INTMAX;
 		for (int j = 0; j < vertexNum; j++) //get the minPath, record the vertex
 		{
@@ -90,25 +118,44 @@ void UndirectedNetwork::allShortestPath(vector<int>& path, vector<int>& shortest
 			}
 		}
 		isFindShortestPath[start] = true;
+		searchMinTime += clock() - tmpTime;
 
-		for (int j = 0; j < vertexNum; j++) //update the shortest path
+		tmpTime = clock();
+		//for (int j = 0; j < vertexNum; j++) //update the shortest path
+		//{
+		//	//if there is no connection between the last added vertex, making it reachable
+		//	//if there is a shorter path to a vertex which alrea_dy have way, make it as the shortest
+		//	//if (!isFindShortestPath[j] && table[start].find(j) != table[start].end() && shortestPath[start] + table[start].find(j)->second < shortestPath[j])
+		//	//{
+		//	//	shortestPath[j] = shortestPath[start] + table[start].find(j)->second;
+		//	//	path[j] = start; //update the path
+		//	//}
+		//
+		//}
+		for (int k = 0; k < table[start].size(); k++)
 		{
-			//if there is no connection between the last added vertex, making it reachable
-			//if there is a shorter path to a vertex which alrea_dy have way, make it as the shortest
-			if (!isFindShortestPath[j] && matrix[start][j] != INTMAX && shortestPath[start] + matrix[start][j] < shortestPath[j])
+			int j = table[start][k].vertexId;
+			if (!isFindShortestPath[j] && shortestPath[start] + table[start][k].weight < shortestPath[j])
 			{
-				shortestPath[j] = shortestPath[start] + matrix[start][j];
-				path[j] = start;	//update the path
+				shortestPath[j] = shortestPath[start] + table[start][k].weight;
+				path[j] = start; //update the path
 			}
 		}
+
+		updateTime += clock() - tmpTime;
 	}
+	cerr << "Search time: " << searchMinTime << "ms" << endl;
+	cerr << "Update time: " << updateTime << "ms" << endl;
 }
-#pragma endregion
 
 int main()
 {
+	vector<int> path;		  //save the previous vertex of the way that to the start vertex
+	vector<int> shortestPath; //save the shortest path from the start vertex to the other vertex
 	UndirectedNetwork un;
+	createTime = clock();
 	un.create();
+	cerr << "Create time: " << clock() - createTime << "ms" << endl;
 	un.allShortestPath(path, shortestPath);
 	for (int i = 0; i < path.size(); i++)
 	{
