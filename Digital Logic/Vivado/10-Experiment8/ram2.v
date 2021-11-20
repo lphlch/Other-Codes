2c0d0ff39bb3fd3b3ce8cffcec3ac30a08d40e22
lphlch <lvpinhao@sohu.com>
2021-11-20 15:33:45 +0800

    Exp8-2
    AC

--- Digital Logic/Vivado/10-Experiment8/ram2.v
+++ Digital Logic/Vivado/10-Experiment8/ram2.v
@@ -1,15 +1,13 @@
-module ram2 (
-    input clk,
-    input ena,
-    input wena,
-    input [4:0] addr,
-    inout [31:0] data
-);
+module ram2 (input clk,
+             input ena,
+             input wena,
+             input [4:0] addr,
+             inout [31:0] data);
     reg [31:0] ram_data [31:0];
     reg [31:0] data_tmp;
     integer i;
     initial begin
-        for ( i = 0;i <= 31;i = i+1 )
+        for (i = 0;i <= 31;i = i+1)
             ram_data[i] <= 0;
     end
     
@@ -18,17 +16,17 @@ module ram2 (
             if (wena) begin
                 ram_data[addr] <= data;
             end
-        end        
-    end
-    always @(*) begin
-        $monitor($time,data,wena,ena,addr,"Data=%h, DataTmp = %h, Ram_data= %h", data,data_tmp,ram_data[addr]);
-        if (ena && wena == 0) begin
-            data_tmp <= ram_data[addr];
+            else begin
+                data_tmp <= ram_data[addr];
+            end
         end
-        else if(ena==0) begin
+    end
+    always @(negedge ena) begin
+        $monitor($time,data,wena,ena,addr,"Data = %h, DataTmp = %h, Ram_data = %h", data,data_tmp,ram_data[addr]);
+        if (ena == 0) begin
             data_tmp <= 32'bz;
         end
     end
-
-    assign data = data_tmp;
-endmodule 
+    
+    assign data = ~wena? data_tmp : 32'bz;
+endmodule
--- Digital Logic/Vivado/10-Experiment8/ram2_tb.v
+++ Digital Logic/Vivado/10-Experiment8/ram2_tb.v
@@ -68,16 +68,3 @@ module ram2_tb();
     end
     
 endmodule
-
-module inout_top
-(
-input       I_data_in,
-inout       IO_data,
-output     O_data_out,
-input       Control
-);
-
-assign IO_data = Control ? I_data_in : 1'bz ;
-assign O_data_out = IO_data ;
-
-endmodule
