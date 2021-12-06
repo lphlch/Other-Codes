9c144f61ed27a9e065653898f55fd51d5e7b97f0
lphlch <lvpinhao@sohu.com>
2021-12-06 18:11:01 +0800

    Fix merge bug

--- Digital Logic/Piano/Test/Test_VGA.v
+++ Digital Logic/Piano/Test/Test_VGA.v
@@ -14,7 +14,7 @@ module VGA_color_line (CLK,
     output [3:0] VGA_GREEN;
     output [3:0] VGA_BLUE;  //像素三基色输出R、G、B
     reg [11:0] rgb_vga;  //4位一组，分别为R、G、B
-    wire clk_vga;  //像素时钟 1688 * 1066 * 108 MZ 
+    wire clk_vga;  //像素时钟 1688 * 1066 * 108 MZ
     
     // Horizontal Parameter(Pixel)
     parameter
@@ -36,64 +36,55 @@ module VGA_color_line (CLK,
     .resetn(RST_N),
     .clk_out1(clk_vga)
     );
-
+    
     //行同步计数
     reg [10:0] hcnt;
     reg VGA_HSYNC;
     always @ (posedge clk_vga or negedge RST_N)
     begin
         if (!RST_N)
-            hcnt <= 0;  //置零
+            hcnt <= 0;  //清零
         else
         begin
-            if (hcnt < H_TOTAL - 1'b1)  //判断扫描完一行
+            if (hcnt < H_TOTAL - 1'b1)  //判断扫描完一行,像素区间为[0, H_TOTAL - 1'b1]
                 hcnt <= hcnt + 1'b1;
             else
-                begin
-                    if (hcnt < H_TOTAL - 1'b1)  //判断扫描完一行,像素区间为[0, H_TOTAL - 1'b1]
-                        hcnt <= hcnt + 1'b1;
-                    else
-                        hcnt <= 0;
-                end
+                hcnt <= 0;
         end
     end
     //行同步
     always @ (posedge clk_vga or negedge RST_N)
     begin
         if (!RST_N)
-            VGA_HSYNC <= 0; //置零
+            VGA_HSYNC <= 0; //清零
         else
         begin
-            //行同步
+            //像素(H_DISP + H_FRONT - 1, H_DISP + H_FRONT + H_SYNC - 1]区间同步
             if (hcnt >= (H_DISP + H_FRONT - 1'b1) && hcnt < (H_DISP + H_FRONT + H_SYNC - 1'b1))
-                VGA_HSYNC <= 1;
+                VGA_HSYNC <= 1;  //在同步区，行同步
             else
-                begin
-                //像素(H_DISP + H_FRONT - 1, H_DISP + H_FRONT + H_SYNC - 1]区间同步
-                    if (hcnt >= (H_DISP + H_FRONT - 1'b1) && hcnt < (H_DISP + H_FRONT + H_SYNC - 1'b1))
-                        hs_vga <= 1;  //在同步区置1，行同步
-                    else
-                        hs_vga <= 0;
-                end
+                VGA_HSYNC <= 0;
         end
     end
     
-    //场同步计数器
+    //场同步计数
     reg [10:0] vcnt;
     reg VGA_VSYNC;
     always @ (posedge clk_vga or negedge RST_N)  //异步复位
     begin
         if (!RST_N)
-            vcnt <= 0;  //置零
+            vcnt <= 0;  //清零
         else
         begin
-            if (hcnt == H_DISP - 1'b1)  //判断扫完一行
+            if (hcnt == H_DISP - 1'b1)  //判断显示区扫面完一行
             begin
-                if (vcnt < V_TOTAL - 1'b1)  //判断扫完一场
+                if (vcnt < V_TOTAL - 1'b1)  //判断扫面完一场
                     vcnt <= vcnt + 1'b1;
                 else
                     vcnt <= 0;
             end
+            else
+                vcnt <= vcnt;
         end
     end
     //场同步
@@ -102,29 +93,28 @@ module VGA_color_line (CLK,
         if (!RST_N)
             VGA_VSYNC <= 0;
         else
-                begin
-                    if (vcnt >= (V_DISP + V_FRONT - 1'b1) && vcnt < (V_DISP + V_FRONT + V_SYNC - 1'b1))
-                        vs_vga <= 1;
-                    else
-                        vs_vga <= 0;
-                end
+        begin
+            if (vcnt >= (V_DISP + V_FRONT - 1'b1) && vcnt < (V_DISP + V_FRONT + V_SYNC - 1'b1))
+                VGA_VSYNC <= 1;
+            else
+                VGA_VSYNC <= 0;
         end
     end
     
-    //坐标xy，在非显示期，坐标为0
+    //坐标在非显示期，坐标置零
     wire [10:0] xpos_vga, ypos_vga;
-    assign xpos_vga = (hcnt < H_DISP) ? (hcnt + 1'b1) : 0;  //横坐标 + 1（即1~H_DISP）
-    assign ypos_vga = (vcnt < V_DISP) ? (vcnt + 1'b1) : 0;  //竖坐标 + 1（即1~V_DISP）
+    assign xpos_vga = (hcnt < H_DISP) ? (hcnt + 1'b1) : 11'd0;  //横坐标 + 1（即1~H_DISP）
+    assign ypos_vga = (vcnt < V_DISP) ? (vcnt + 1'b1) : 11'd0;  //竖坐标 + 1（即1~V_DISP）
     
-    //显示像素
+    //显示
     always @ (posedge clk_vga or negedge RST_N)
     begin
         if (!RST_N)
             rgb_vga <= 0;
         else
         begin
-/*            测试彩条
-            if (xpos_vga > 0 && xpos_vga      <= 80) rgb_vga      <= 9'b111111111;//white
+            /*            测试彩条
+             if (xpos_vga > 0 && xpos_vga        <= 80) rgb_vga        <= 9'b111111111;//white
              else if (xpos_vga > 80 && xpos_vga  <= 160) rgb_vga  <= 9'b111000000;//red
              else if (xpos_vga > 160 && xpos_vga <= 240) rgb_vga <= 9'b111000111;//mangenta
              else if (xpos_vga > 240 && xpos_vga <= 320) rgb_vga <= 9'b111111000;//yellow
@@ -141,19 +131,19 @@ module VGA_color_line (CLK,
              else if (xpos_vga > 1040 && xpos_vga <= 1120) rgb_vga <= 9'b000000111;//blue
              else if (xpos_vga > 1120 && xpos_vga <= 1200) rgb_vga <= 9'b000111111;//cyan
              else if (xpos_vga > 1200 && xpos_vga <= 1280) rgb_vga <= 9'b000000000;
-             else rgb_vga                         <= 0;//black，这个很重要，不然颜色不怎么正常 
+             else rgb_vga                         <= 0;//black，这个很重要，不然颜色不怎么正常
              */
-
+            
             //显示像素
             if (xpos_vga > 0 && xpos_vga <= 1280) begin
                 rgb_vga <= RGB;
             end
             else
-                rgb_vga <= 0; 
-                
+                rgb_vga <= 0;
+            
         end
     end
-    //三基色分离
+    //分离颜色
     assign VGA_RED   = rgb_vga[11:8];
     assign VGA_GREEN = rgb_vga[7:4];
     assign VGA_BLUE  = rgb_vga[3:0];
