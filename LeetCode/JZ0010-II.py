63e1371e8022350a7107836fb2f55565d63ad6b7
lphlch <lvpinhao@sohu.com>
2022-03-30 21:30:16 +0800

    Add comments

--- LeetCode/JZ0010-II.py
+++ LeetCode/JZ0010-II.py
@@ -1,3 +1,12 @@
+'''
+剑指 Offer 10- II. 青蛙跳台阶问题
+一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+注意：本题与主站 70 题相同：https://leetcode-cn.com/problems/climbing-stairs/
+'''
+
 class Solution:
     def numWays(self, n: int) -> int:
         if(n == 0):
--- LeetCode/LC0070.py
+++ LeetCode/LC0070.py
@@ -1,3 +1,14 @@
+'''
+70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+70. 爬楼梯
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+'''
 class Solution:
     def climbStairs(self, n: int) -> int:
         count=[0]*(n+1)     # init count list
