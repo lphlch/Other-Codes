635a4f3698a5c96ba456d4e7c6dac8f9e419b2fa
lphlch <lvpinhao@sohu.com>
2022-05-19 16:30:54 +0800

    Create LC0066.py

---
+++ LeetCode/LC0066.py
@@ -0,0 +1,20 @@
+'''
+66. 加一
+给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+66. Plus One
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+'''
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num=int(''.join(map(str,digits)))
+        num+=1
+        return list(map(int,str(num)))
